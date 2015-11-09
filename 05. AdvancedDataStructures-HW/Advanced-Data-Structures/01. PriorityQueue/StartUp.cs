25b34e548733a048d52e4eb1b41a9bdba5f14b26
zdzdz <zdzdz.link@yahoo.com>
2015-11-10 00:06:45 +0200

    Advanced Data Structures

---
+++ 05. AdvancedDataStructures-HW/Advanced-Data-Structures/01. PriorityQueue/StartUp.cs
@@ -0,0 +1,28 @@
+﻿//// Implement a class PriorityQueue<T> based on the data structure "binary heap".
+
+namespace PriorityQueue
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            PriorityQueue<Person> people = new PriorityQueue<Person>();
+            people.Enquene(new Person("Samuel", 25));
+            people.Enquene(new Person("Sancho", 27));
+            people.Enquene(new Person("Grigor", 30));
+            people.Enquene(new Person("Asya", 24));
+            people.Enquene(new Person("Gergana", 35));
+            people.Enquene(new Person("Maria", 22));
+            people.Enquene(new Person("Pesho", int.MinValue));
+
+            Console.WriteLine("The root: {0}", people.Root);
+
+            while (people.Count > 0)
+            {
+                Console.WriteLine(people.Dequene());
+            }
+        }
+    }
+}
---
+++ 05. AdvancedDataStructures-HW/Advanced-Data-Structures/02. ProductsCollection/RandomGenerator.cs
@@ -0,0 +1,39 @@
+﻿namespace ProductsCollection
+{
+    using System;
+
+    public static class RandomGenerator
+    {
+        private const string Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        private static readonly Random Random = new Random();
+
+        public static decimal RandomDecimalBetween(double minValue, double maxValue)
+        {
+            var next = Random.NextDouble();
+
+            return (decimal)(minValue + (next * (maxValue - minValue)));
+        }
+
+        public static int GetRandomNumber(int min, int max)
+        {
+            return Random.Next(min, max + 1);
+        }
+
+        public static string GetRandomString(int length)
+        {
+            var result = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = Letters[GetRandomNumber(0, Letters.Length - 1)];
+            }
+
+            return new string(result);
+        }
+
+        public static string GetRandomStringWithRandomLength(int min, int max)
+        {
+            return GetRandomString(GetRandomNumber(min, max));
+        }
+    }
+}
