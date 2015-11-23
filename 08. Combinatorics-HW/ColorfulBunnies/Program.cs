1d7a84f6b1c8c0bad0e5f17249181bc24b98537a
zdzdz <zdzdz.link@yahoo.com>
2015-11-23 21:42:16 +0200

    Combinatorics homework

---
+++ 08. Combinatorics-HW/ColorfulBunnies/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace ColorfulBunnies
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int numberOfRabits = int.Parse(Console.ReadLine());
+            var responses = new int[numberOfRabits];
+
+            for (int i = 0; i < numberOfRabits; i++)
+            {
+                responses[i] = int.Parse(Console.ReadLine());
+            }
+
+            var result = GetMinimum(responses);
+            Console.WriteLine(result);
+        }
+
+        public static int GetMinimum(int[] responses)
+        {
+            int[] cache = new int[1000002];
+            for (int i = 0; i < responses.Length; i++)
+            {
+                cache[responses[i] + 1]++;
+            }
+
+            int minCount = 0;
+            for (int i = 0; i < cache.Length; i++)
+            {
+                if (cache[i] == 0)
+                {
+                    continue;
+                }
+                if (cache[i] <= i)
+                {
+                    minCount += i;
+                }
+                else
+                {
+                    minCount += (int) Math.Ceiling((double) cache[i]/i)*i;
+                }
+            }
+
+            return minCount;
+        }
+    }
+}
